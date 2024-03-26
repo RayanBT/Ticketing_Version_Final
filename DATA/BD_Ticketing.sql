f6915398731f771bba686778069de52a510c89ee
Rayan <btrayan21@gmail.com>
2024-03-26 22:41:49 +0100

    Update project

--- DATA/BD_Ticketing.sql
+++ DATA/BD_Ticketing.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : jeu. 15 fév. 2024 à 14:22
+-- Hôte : localhost
+-- Généré le : mar. 26 mars 2024 à 22:40
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -18,8 +18,23 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `bd_ticketing`
+-- Base de données : `BD_Ticketing`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `actions_tickets`
+--
+
+CREATE TABLE `actions_tickets` (
+  `id_action` int(11) NOT NULL,
+  `id_ticket` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `date_action` datetime NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `justification` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -49,19 +64,6 @@ INSERT INTO `libelle_ticket` (`id_libelle`, `libelle`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `log`
---
-
-CREATE TABLE `log` (
-  `id_ticket` int(11) NOT NULL,
-  `login` int(32) NOT NULL,
-  `contenu` text NOT NULL,
-  `technicien` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `tickets`
 --
 
@@ -75,7 +77,7 @@ CREATE TABLE `tickets` (
   `priorite` enum('Faible','Moyen','Important','Urgent') NOT NULL,
   `date_creation` date NOT NULL,
   `statut` enum('Ouvert','En cours','Fermé') NOT NULL DEFAULT 'Ouvert',
-  `technicien` varchar(32) NOT NULL DEFAULT 'Personne'
+  `technicien` varchar(32) NOT NULL DEFAULT 'Non assigné'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -83,37 +85,9 @@ CREATE TABLE `tickets` (
 --
 
 INSERT INTO `tickets` (`id_ticket`, `login`, `id_libelle`, `description`, `salle`, `ip`, `priorite`, `date_creation`, `statut`, `technicien`) VALUES
-(45, 'armand', 9, '(\"y\"\'r', 'G22', '192.168.1.60', 'Faible', '2024-02-11', 'En cours', 'armand');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `tickets_close`
---
-
-CREATE TABLE `tickets_close` (
-  `id_ticket` int(11) NOT NULL,
-  `login` varchar(32) NOT NULL,
-  `id_libelle` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `priorite` enum('Faible','Moyen','Important','Urgent') NOT NULL,
-  `date_creation` date NOT NULL,
-  `date_fermeture` date NOT NULL DEFAULT current_timestamp(),
-  `statut` enum('Ouvert','En cours','Fermé') NOT NULL,
-  `technicien` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `tickets_close`
---
-
-INSERT INTO `tickets_close` (`id_ticket`, `login`, `id_libelle`, `description`, `priorite`, `date_creation`, `date_fermeture`, `statut`, `technicien`) VALUES
-(33, 'rayan', 9, 'test', 'Faible', '2024-01-11', '2024-01-11', 'Fermé', 'tech1'),
-(34, 'rayan', 4, 'toita', 'Faible', '2024-01-11', '2024-01-11', 'Fermé', 'tech1'),
-(35, 'rayan', 9, 'sdfdf\r\n\r\n', 'Faible', '2024-01-11', '2024-01-11', 'Fermé', 'tech1'),
-(36, 'rayan', 4, 'qfqfqf', 'Important', '2024-01-11', '2024-01-12', 'Fermé', 'tech2'),
-(37, 'rayan', 8, 'blablabla', 'Important', '2024-01-12', '2024-01-12', 'Fermé', 'tech1'),
-(39, 'rayan', 9, 'zfaf', 'Faible', '2024-01-12', '2024-01-12', 'Fermé', 'tech1');
+(59, 'rayan', 9, 'IPIPIP', 'G22', '::1', 'Faible', '2024-03-26', 'Fermé', 'tech1'),
+(60, 'rayan', 9, 'vlbqdfq', 'G22', '::1', 'Faible', '2024-03-26', 'Ouvert', 'Non assigné'),
+(61, 'rayan', 9, 'lbqdvvc', 'G26', '::1', 'Faible', '2024-03-26', 'Ouvert', 'Non assigné');
 
 -- --------------------------------------------------------
 
@@ -135,19 +109,21 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nom`, `login`, `email`, `mdp`, `user_role`) VALUES
-(9, 'ryry', 'ryry', 'ryry@gmail.com', '3f1c4215f8cb', 'utilisateur'),
 (11, 'Gestion', 'gestion', 'gestion@gestion.com', '3f1c4215f8cb', 'admin_web'),
-(12, 'tech1', 'tech1', 'tech1@gmail.com', '3f1c4215f8cb', 'technicien'),
-(13, 'rayan', 'rayan', 'rayan@gmail.com', '3f1c4215f8cb', 'utilisateur'),
-(14, 'tech2', 'tech2', 'tech2@gmail.com', '3f1c4215f8cb', 'technicien'),
-(16, 'admin système', 'admin', 'admin_sys@gmail.com', '2a095308', 'admin_systeme'),
-(17, 'Polo', 'tech3', 'tech3@gmail.com', '3f1c4215f8cb', 'technicien'),
-(18, 'Armand', 'armand', 'armand@gmail.fr', '3f1c4215f8cb', 'utilisateur'),
-(19, 'test', 'test2', 'test@outlook.com', '2a035413', 'utilisateur');
+(12, 'tec1', 'tec1', 'tec1@gmail.com', '3f1c4215f8cb', 'technicien'),
+(14, 'tec2', 'tec2', 'tec2@gmail.com', '3f1c4215f8cb', 'technicien'),
+(16, 'admin système', 'admin', 'admin_sys@gmail.com', '3f1c4215f8cb', 'admin_systeme'),
+(17, 'tec3', 'tec3', 'tec3@gmail.com', '3f1c4215f8cb', 'technicien');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `actions_tickets`
+--
+ALTER TABLE `actions_tickets`
+  ADD PRIMARY KEY (`id_action`);
 
 --
 -- Index pour la table `libelle_ticket`
@@ -157,21 +133,9 @@ ALTER TABLE `libelle_ticket`
   ADD UNIQUE KEY `libelle` (`libelle`);
 
 --
--- Index pour la table `log`
---
-ALTER TABLE `log`
-  ADD PRIMARY KEY (`id_ticket`);
-
---
 -- Index pour la table `tickets`
 --
 ALTER TABLE `tickets`
-  ADD PRIMARY KEY (`id_ticket`);
-
---
--- Index pour la table `tickets_close`
---
-ALTER TABLE `tickets_close`
   ADD PRIMARY KEY (`id_ticket`);
 
 --
@@ -186,28 +150,28 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `actions_tickets`
+--
+ALTER TABLE `actions_tickets`
+  MODIFY `id_action` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT pour la table `libelle_ticket`
 --
 ALTER TABLE `libelle_ticket`
   MODIFY `id_libelle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT pour la table `log`
---
-ALTER TABLE `log`
-  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
